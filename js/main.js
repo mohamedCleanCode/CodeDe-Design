cd88d92e9a78c11d55807023e161cec4da8a7964
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-10-18 23:41:04 +0200

    Edit js file

--- js/main.js
+++ js/main.js
@@ -10,12 +10,67 @@ icon.onclick = function () {
 
 // Colors List
 let colorsList = document.querySelectorAll(".colors-list li");
+if (localStorage.getItem("color")) {
+  document.documentElement.style.setProperty(
+    "--main-color",
+    localStorage.getItem("color")
+  );
+  colorsList.forEach((li) => {
+    li.classList.remove("active");
+  });
+  document
+    .querySelector(`[data-color="${localStorage.getItem("color")}"]`)
+    .classList.add("active");
+}
 colorsList.forEach((li) => {
   li.addEventListener("click", (e) => {
     document.documentElement.style.setProperty(
       "--main-color",
       e.target.dataset.color
     );
+    localStorage.setItem("color", e.target.dataset.color);
+    colorsList.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+
+// Random Backgrounds
+let randomBg = document.querySelectorAll(".random-bg button");
+
+let randonBgOpt = true;
+let counter;
+if (localStorage.getItem("background")) {
+  if (localStorage.getItem("background") === "true") {
+    randonBgOpt = true;
+  } else {
+    randonBgOpt = false;
+  }
+  randomBg.forEach((button) => {
+    button.classList.remove("active");
+  });
+  if (localStorage.getItem("background") === "true") {
+    document.querySelector(".random-bg .yes").classList.add("active");
+  } else {
+    document.querySelector(".random-bg .no").classList.add("active");
+  }
+}
+randomBg.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    randomBg.forEach((button) => {
+      button.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    if (e.target.dataset.choice === "yes") {
+      randonBgOpt = true;
+      randomImages();
+      localStorage.setItem("background", true);
+    } else {
+      randonBgOpt = false;
+      clearInterval(counter);
+      localStorage.setItem("background", false);
+    }
   });
 });
 
@@ -23,7 +78,12 @@ colorsList.forEach((li) => {
 
 let landingPage = document.querySelector(".landing-page");
 let imgsOfArray = ["1bg.jpg", "2bg.jpg", "3bg.jpg", "4bg.jpg"];
-let counter = setInterval(() => {
-  let randomNumber = Math.floor(Math.random() * imgsOfArray.length);
-  landingPage.style.backgroundImage = `url(imgs/${imgsOfArray[randomNumber]})`;
-}, 3000);
+function randomImages() {
+  if (randonBgOpt === true) {
+    counter = setInterval(() => {
+      let randomNumber = Math.floor(Math.random() * imgsOfArray.length);
+      landingPage.style.backgroundImage = `url(imgs/${imgsOfArray[randomNumber]})`;
+    }, 3000);
+  }
+}
+randomImages();
