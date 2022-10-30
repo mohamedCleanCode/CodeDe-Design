d87700b0bb0b7cbd6055923b4a16f8ae35cdd843
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-10-31 01:05:31 +0200

    Edit js file
    add some logic

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ icon.onclick = function () {
 
 // Colors List
 let colorsList = document.querySelectorAll(".colors-list li");
-if (localStorage.getItem("color")) {
+if (localStorage.getItem("color") !== null) {
   document.documentElement.style.setProperty(
     "--main-color",
     localStorage.getItem("color")
@@ -28,6 +28,7 @@ colorsList.forEach((li) => {
       "--main-color",
       e.target.dataset.color
     );
+
     localStorage.setItem("color", e.target.dataset.color);
     colorsList.forEach((li) => {
       li.classList.remove("active");
@@ -41,7 +42,7 @@ let randomBg = document.querySelectorAll(".random-bg button");
 
 let randonBgOpt = true;
 let counter;
-if (localStorage.getItem("background")) {
+if (localStorage.getItem("background") !== null) {
   if (localStorage.getItem("background") === "true") {
     randonBgOpt = true;
   } else {
@@ -74,6 +75,39 @@ randomBg.forEach((button) => {
   });
 });
 
+// Bullets
+let bulletsOpt = document.querySelectorAll(".bullets-opt button");
+let bulletsContainer = document.querySelector(".bullets");
+
+if (localStorage.getItem("bullets_option") !== null) {
+  bulletsOpt.forEach((el) => {
+    el.classList.remove("active");
+  });
+  if (localStorage.getItem("bullets_option") === "block") {
+    bulletsContainer.style.display = "block";
+    document.querySelector(".bullets-opt .yes").classList.add("active");
+  } else {
+    bulletsContainer.style.display = "none";
+    document.querySelector(".bullets-opt .no").classList.add("active");
+  }
+}
+
+bulletsOpt.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    if (el.dataset.display === "yes") {
+      bulletsContainer.style.display = "block";
+      localStorage.setItem("bullets_option", "block");
+    } else if (el.dataset.display === "no") {
+      bulletsContainer.style.display = "none";
+      localStorage.setItem("bullets_option", "none");
+    }
+    bulletsOpt.forEach((el) => {
+      el.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+
 // [2] --> Logic Of Lnading Page
 
 let landingPage = document.querySelector(".landing-page");
@@ -93,10 +127,7 @@ randomImages();
 let links = document.querySelectorAll(".links li a");
 links.forEach((a) => {
   a.addEventListener("click", (e) => {
-    links.forEach((a) => {
-      a.classList.remove("active");
-      e.target.classList.add("active");
-    });
+    handelActiveClass(e);
   });
 });
 
@@ -147,3 +178,37 @@ addEventListener("click", (e) => {
   }
 });
 
+// [6] Logic Of Bullets
+
+let allBullets = document.querySelectorAll(".bullets .bullet");
+
+allBullets.forEach((bullet) => {
+  bullet.addEventListener("click", (e) => {
+    document.querySelector(e.target.dataset.section).scrollIntoView({
+      behavior: "smooth",
+    });
+    allBullets.forEach((bullet) => {
+      bullet.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+
+// [7] Reset Options
+document.querySelector(".reset-box").addEventListener("click", (e) => {
+  localStorage.clear();
+  location.reload();
+});
+
+// [8] Toggle Menu
+let toggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu ul");
+toggle.onclick = () => {
+  menu.classList.toggle("open");
+};
+document.addEventListener("click", (e) => {
+  if (e.target !== toggle && menu.classList.contains("open")) {
+    menu.classList.toggle("open");
+    console.log("true");
+  }
+});
